2af770aaf2056d48a98fb04f24c6f208840f6a89
jo732169 <156470466+jo732169@users.noreply.github.com>
2024-01-30 19:46:18 -0500

    Add files via upload

---
+++ src/test/test_assignment_1.py
@@ -0,0 +1,24 @@
+import unittest
+import sys
+from pathlib import Path
+
+# Add the parent directory to sys.path to import the main module
+parent_dir = str(Path(__file__).parent.parent)
+sys.path.append(parent_dir)
+from main.assignment_1 import ApproximationAlgorithm, BisectionMethod, FixedPointIteration, NewtonRaphsonMethod
+
+def test():
+    print("Testing Approximation Algorithm:")
+    print(ApproximationAlgorithm())
+
+    print("\nTesting Bisection Method:")
+    print(BisectionMethod())
+
+    print("\nTesting Fixed Point Iteration")
+    print(FixedPointIteration())
+
+    print("\nTesting Newton Raphson Method")
+    print(NewtonRaphsonMethod())
+
+if __name__ == '__main__':
+    test()
